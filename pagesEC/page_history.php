f782df3023c9aae63ca33dc422938cb17cecd97c
Jeff Risberg <jrisberg@aol.com>
2012-11-06 08:44:31 -0800

    added example of using highcharts to show history of page creation

---
+++ pagesEC/page_history.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Twitter Bootstrap</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Jeff Risberg">
+  
+    <link href="../css/bootstrap.css" rel="stylesheet">   
+    <link href="../css/bootstrap-responsive.css" rel="stylesheet">
+    
+    <script src="../js/jquery-1.8.2.min.js"></script>
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/less-1.3.0.min.js"></script>
+    <script src="../js/highcharts.js"></script>
+    
+    <link href="../css/stylesEC.less" rel="stylesheet">
+    
+    <style type="text/css">
+      body {
+        padding-top: 40px;
+        padding-bottom: 10px;
+      }  
+      hr { 
+        height: 3px; color: #888 !important; background-color: #888 !important;
+      }   
+      .solution {
+        background: #eee;
+        border-radius: 5px;
+      }
+    </style>    
+
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+  
+    <link rel="shortcut icon" href="../ico/favicon.ico">
+  </head>
+
+  <body>
+  
+    <div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">         
+          <a class="brand" href="../index.html">TB01</a>
+          <div class="nav-collapse collapse">            
+            <form class="navbar-form pull-right">
+              <input class="span2" type="text" placeholder="Email">
+              <input class="span2" type="password" placeholder="Password">
+              <button type="submit" class="btn">Sign in</button>
+            </form>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+    </div>        
+    
+    <div class="container">
+    
+      <div style="margin-bottom: 10px">
+        <img width="900" height="100" src="img/ecocloud_header.png" />      
+      </div>
+    
+      <div class="row-fluid">
+        <div class="span12">     
+	        <div class="navbar">
+			      <div class="navbar-inner">
+			        <a class="brand" href="#"></a>
+			        <ul class="nav">
+			          <li><a href="#">Home</a></li>
+			          <li class="divider-vertical"></li>
+			          <li class="active"><a href="#">Topics</a></li>
+			          <li class="divider-vertical"></li>
+			          <li><a href="#">Marketplace</a></li>
+			          <li class="divider-vertical"></li>
+			          <li class="dropdown">
+		               <a href="#" class="dropdown-toggle" data-toggle="dropdown">Collaboration <b class="caret"></b></a>
+		               <ul class="dropdown-menu">
+		                 <li><a href="#">Action</a></li>
+		                 <li><a href="#">Another action</a></li>
+		                 <li class="divider"></li>
+		                 <li class="nav-header">Nav header</li>
+		                 <li><a href="#">Separated link</a></li>
+		                 <li><a href="#">One more separated link</a></li>
+		               </ul>
+			          </li>
+			          <li class="divider-vertical"></li>
+			          <li class="dropdown">
+			            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Solution <b class="caret"></b></a>
+			            <ul class="dropdown-menu">
+			              <li><a href="#">contribute</a></li>
+			              <li><a href="#">find</a></li>
+			            </ul>
+			          </li>
+			          <li class="divider-vertical"></li>
+			          <form class="navbar-search pull-left">
+			            <input type="text" class="search-query" placeholder="Search">
+			          </form> 
+			        </ul>
+			      </div>
+			    </div>
+			  </div>
+			</div>	    
+   
+      <div class="row-fluid">
+	      <div class="span9">      
+		      
+	        <h4>History of Topic Page Creation and Update</h4>   
+	        <div id="chart1" style="width: 800px; height: 400px;"></div>
+	        
+        </div><!--/span-->
+	    </div><!-- /row -->  
+
+      <hr>
+
+      <footer>      
+       <div class="row">
+         <div class="span12">
+           <div class="pull-left">
+             &copy; 2010 - 2012 by Sustainable Silicon Valley. All Rights Reserved.
+           </div>
+           <div class="pull-right">        
+             About Us | Sign Up for Newsletter | Donate | Contact Us | Help | Terms of Service
+           </div>
+         </div>
+       </div>    
+      </footer>
+
+    </div><!--/.container-->
+    
+    <script type="text/javascript">
+      $(function() {
+
+        var chart = new Highcharts.Chart({
+           chart: {
+             renderTo: 'chart1',
+             type: 'column'
+           },
+           title: {
+             text: 'Pages Created or Updated'
+           },          
+           xAxis: {
+             categories: [
+                 'Older',
+                 '6 Weeks ago',
+                 '5 Weeks ago',
+                 '4 Week ago',
+                 '3 Weeks ago',
+                 '2 Weeks ago',
+                 '1 Week ago',
+                 'This Week',             
+             ]
+           },
+           yAxis: {
+             min: 0,
+             title: {
+                 text: 'Count'
+             }
+           },   
+           credits: {
+        	    enabled: false
+           },
+           plotOptions: {
+                  column: {
+                      pointPadding: 0.2,
+                      borderWidth: 0
+                  }
+           },
+           series: [{
+                  name: 'Created',
+                  data: [25, 19, 5, 0, 3, 3, 1, 1]      
+              }, {
+                  name: 'Updated',
+                  data: [14, 18, 12, 3, 2, 5, 3, 2]           
+              }]
+          });
+      });
+    </script>
+  </body>
+</html>
+ 
