a0bc325a409e51cb0daa05532120b90c6d4fccc3
Jeff Risberg <jrisberg@aol.com>
2012-11-16 00:44:45 -0800

    resized a box

--- pagesEC/blog_post_detail.php
+++ pagesEC/blog_post_detail.php
@@ -233,7 +233,7 @@
 		            <div class="comment">On Nov 16, 2012 Mary Jones said: Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</div>
 		            <div style="margin-top: 4px; background: #eee; padding: 5px">
 		              <form style="margin: 0px">		          
-				            <textarea style="width: 640px; height: 80px"></textarea>
+				            <textarea style="width: 650px; height: 70px"></textarea>
 				            <br>
 				            <input type="submit" value="Add Comment" />
 			            </form>
